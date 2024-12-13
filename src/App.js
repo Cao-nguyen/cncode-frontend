b61a227c58f2f71e29d478e332fc7d7a4c6966b9
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-12-13 09:04:48 +0700

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mt-5 text-primary">
+        <h1 className="text-center">DỰ ÁN CNCODE</h1>
+        <h3 className="text-black text-center">Được phát triển bởi Lý Cao Nguyên</h3>
+        <div className="col text-center">
+          <a href='https://chinhphucnguvanedu.vercel.app' target="_blank" rel="noopener noreferrer" className="btn btn-primary">Chinh Phục Ngữ Văn</a>
+        </div>
+      </div>
     </div>
   );
 }
