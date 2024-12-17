b99e4066f6b95f47fe2ef1c66fa6150ca02fafeb
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-12-17 11:00:27 +0700

    c

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
+import Trangchu from './components/Home/Trangchu';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/">
-
-        </Route>
+        <Route path="/" Component={Trangchu}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/Home/Trangchu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Trangchu(props) {
+    return (
+        <div className="container">
+            <h1 className="mt-3 text-center text-primary">CNcode - Dạy học lập trình miễn phí</h1>
+            <h2 className="mt-3 text-center">Dự án đang được phát triển bởi Lý Cao Nguyên</h2>
+        </div>
+    );
+}
+
+export default Trangchu;
