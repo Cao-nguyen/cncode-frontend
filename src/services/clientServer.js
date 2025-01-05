db523563ca8a6b63865a3ab639f32e006652819a
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-05 14:22:02 +0700

    Update clientServer.js

--- src/services/clientServer.js
+++ src/services/clientServer.js
@@ -2,13 +2,13 @@ import axios from "axios"
 
 // API xác thực
 export const checkCode = async (email) => {
-    const response = await axios.post('http://localhost:8080/api/v1/xacthuc', { email });
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/xacthuc`, { email });
     return response.data
 };
 
 // API Đăng ký
 export const registerUser = async (fullName, email, username, password, code) => {
-    const response = await axios.post('http://localhost:8080/api/v1/dangky', {
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/dangky`, {
         fullName, email, username, password, code
     })
     return response.data
@@ -16,7 +16,7 @@ export const registerUser = async (fullName, email, username, password, code) =>
 
 // API Đăng nhập
 export const LoginUser = async (fullName, username, password) => {
-    const response = await axios.post('http://localhost:8080/api/v1/dangnhap', {
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/dangnhap`, {
         fullName, username, password
     })
     return response.data
