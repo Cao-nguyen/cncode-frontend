e7b501aa98e09e3766b11e30ccae8e6a0b6f428f
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-05 14:48:11 +0700

    Update adminServer.js

--- src/services/adminServer.js
+++ src/services/adminServer.js
@@ -4,20 +4,12 @@ const backend = process.env.REACT_APP_BACKEND
 
 // Giới thiệu
 export const InforApi = async (Infor) => {
-<<<<<<< Updated upstream
-    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/infor`, { Infor });
-=======
     const response = await axios.post(`${backend}/api/v1/infor`, { Infor });
->>>>>>> Stashed changes
     return response.data
 };
 
 // Giới thiệu
 export const getInforApi = async () => {
-<<<<<<< Updated upstream
-    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/api/v1/infor`);
-=======
     const response = await axios.get(`${backend}/api/v1/infor`);
->>>>>>> Stashed changes
     return response.data
 };
