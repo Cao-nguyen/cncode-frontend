6c92df5d26f9858beb92af0d1ce48fd2a6724cc0
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-05 14:20:45 +0700

    Update adminServer.js

--- src/services/adminServer.js
+++ src/services/adminServer.js
@@ -2,12 +2,12 @@ import axios from "axios"
 
 // Giới thiệu
 export const InforApi = async (Infor) => {
-    const response = await axios.post('http://localhost:8080/api/v1/infor', { Infor });
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/infor`, { Infor });
     return response.data
 };
 
 // Giới thiệu
 export const getInforApi = async () => {
-    const response = await axios.get('http://localhost:8080/api/v1/infor');
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/api/v1/infor`);
     return response.data
 };
