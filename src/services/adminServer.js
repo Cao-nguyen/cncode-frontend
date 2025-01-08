688500735f0480b513fb9e316adf9a7a78a7fa67
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-08 10:19:03 +0700

    o

--- src/services/adminServer.js
+++ src/services/adminServer.js
@@ -1,15 +1,13 @@
 import axios from "axios"
 
-const backend = process.env.REACT_APP_BACKEND
-
 // Giới thiệu
 export const InforApi = async (Infor) => {
-    const response = await axios.post(`${backend}/api/v1/infor`, { Infor });
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND}/api/v1/infor`, { Infor });
     return response.data
 };
 
 // Giới thiệu
 export const getInforApi = async () => {
-    const response = await axios.get(`${backend}/api/v1/infor`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/api/v1/infor`);
     return response.data
 };
