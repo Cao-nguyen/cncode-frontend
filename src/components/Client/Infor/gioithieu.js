d35a07848cfaa1b0153c80ea4aba3013b119ed54
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-09 08:44:55 +0700

    x

--- src/components/Client/Infor/gioithieu.js
+++ src/components/Client/Infor/gioithieu.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 import SettingsAdmin from '../../../middlewares/SettingsAdmin'
 
 function Gioithieu(props) {
-    const {
-        Infor,
-        getInfor
-    } = SettingsAdmin()
+    const { Infor, getInfor } = SettingsAdmin();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getInfor();
+        const fetchData = async () => {
+            setLoading(true);
+            await getInfor();
+            setLoading(false);
+        };
+        fetchData();
     }, [getInfor]);
 
     return (
         <div className="container pt-5">
-            <div className="pt-5" dangerouslySetInnerHTML={{ __html: Infor }}></div>
+            {loading ? (
+                <div className="pt-5">Đang tải...</div>
+            ) : (
+                <div className="pt-5" dangerouslySetInnerHTML={{ __html: Infor }}></div>
+            )}
         </div>
     );
 }
