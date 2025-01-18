80c3c338da1f76493c64724eb64ad71bfccc6fa1
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-18 09:58:08 +0700

    Update Footer.js

--- src/components/Client/Footer/Footer.js
+++ src/components/Client/Footer/Footer.js
@@ -59,7 +59,7 @@ function Footer(props) {
                         </a>
                         <a className="mx-2 links" href="https://www.youtube.com/@cncode-edu" target="_blank" rel="noopener noreferrer">Youtube: CNcode</a>
                     </div>
-                    <a href="https://www.dmca.com/compliance/cncode.vercel.app" title="DMCA Compliance information for cncode.vercel.app"><img src="https://www.dmca.com/img/dmca-compliant-grayscale.png" alt="DMCA compliant image" /></a>
+                    <a href="//www.dmca.com/Protection/Status.aspx?ID=9ef5d4b7-b06d-42a8-9a58-3921ee8b38c9" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=9ef5d4b7-b06d-42a8-9a58-3921ee8b38c9"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
                 </div>
             </div>
         </footer >
