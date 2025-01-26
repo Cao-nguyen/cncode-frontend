7d43b1f085b529968e8f4840c873bf011958b2e6
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-26 12:00:14 +0700

    update 26.1.2025

--- src/components/Client/Tintuc/Tintuc.js
+++ src/components/Client/Tintuc/Tintuc.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 import './Tintuc.scss'
 
 function Tintuc(props) {
+    const [isVisible, setIsVisible] = useState(false);
 
     const [news, setNews] = useState([])
 
@@ -12,6 +13,7 @@ function Tintuc(props) {
         const newsData = async () => {
             const data = await ShowNewClient()
             setNews(data.DT)
+            setIsVisible(true);
         }
 
         newsData()
@@ -19,28 +21,21 @@ function Tintuc(props) {
 
     return (
         <div className="container">
-            {news && news.length > 0 ? (
-                <div className="news">
-                    {news.map(item => (
-                        <div className="news-item" key={item._id}>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                            <div className="news-text">
-                                <p>{item.fullName}</p>
-                                <p>{moment(item.createdAt).format('DD/MM/YYYY')}</p>
-                            </div>
-                            <div className="btn btn-primary">
-                                <Link to={`/tintuc/${item.slug}`}>Xem thêm</Link>
-                            </div>
+            <div className={`news fade-in ${isVisible ? 'visible' : ''}`}>
+                {news.map(item => (
+                    <div className="news-item" key={item._id}>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                        <div className="news-text">
+                            <p>{item.fullName}</p>
+                            <p>{moment(item.createdAt).format('DD/MM/YYYY')}</p>
                         </div>
-                    ))}
-                </div>
-            ) : (
-                <div style={{ height: "100vh" }}>
-                    <h4 className="mt-5 text-center text-primary">Không có tin tức nào được đăng tải</h4>
-                </div>
-            )
-            }
+                        <div className="btn btn-primary">
+                            <Link to={`/tintuc/${item.slug}`}>Xem thêm</Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div >
     );
 }
