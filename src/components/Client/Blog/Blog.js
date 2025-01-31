7024fac59e803585a7a5f3acdfb724ac99d13b51
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-01-31 20:30:20 +0700

    update 31.1.2025

--- src/components/Client/Blog/Blog.js
+++ src/components/Client/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Cert from '../../Client/cert'
 
 function Blog(props) {
     return (
         <div className="container">
-
+            <Cert />
         </div>
     );
 }
