8ec67e32668ea50a2e0be6c647f4193e06ed7219
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-02-04 20:56:12 +0700

    push 4.2.2025

--- src/components/Client/Home/Home.js
+++ src/components/Client/Home/Home.js
@@ -1,27 +1,53 @@
-import React from 'react';
+import React from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 function Home(props) {
-    return (
-        <div className="container">
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>CNcode | Trang chủ </title>
+          <meta
+            name="description"
+            content="Nền tảng học công nghệ thông tin onlineonline"
+          />
+          <link rel="canonical" href="https://cncode.vercel.app" />
+        </Helmet>
+      </HelmetProvider>
 
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+      <div className="container">
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
 
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
 
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
 
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
 
-            <h1 className="text-center mt-5 text-primary">CNcode - Nền tảng học lập trình miễn phí</h1>
-            <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
-        </div>
-    );
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+
+        <h1 className="text-center mt-5 text-primary">
+          CNcode - Nền tảng học lập trình miễn phí
+        </h1>
+        <h3 className="text-center">Dự án được phát triển bởi Lý Cao Nguyên</h3>
+      </div>
+    </>
+  );
 }
 
 export default Home;
