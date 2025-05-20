84ff4bab6944fd738cd594ad7436456d2f165d89
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-05-20 08:47:58 +0700

    push 20.5.2025

--- src/components/Client/Luyentap/Luyentap.js
+++ src/components/Client/Luyentap/Luyentap.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 function Luyentap(props) {
   return (
@@ -16,7 +17,15 @@ function Luyentap(props) {
         </Helmet>
       </HelmetProvider>
       <div className="container">
-        <p className="text-center mt-5">Tính năng đang được phát triển...</p>
+        <p className="text-center mt-5">Do việc học tập nên admin chưa có thời gian phát triển tính năng này.</p>
+        <p className="text-center mt-5">Khoá học khai giảng đầu tiên bắt đầu vào ngày 15/6/2025</p>
+        <p className="text-center mt-5">Dự kiến tính năng luyện tập được hoàn thành trước ngày 15/6/2025</p>
+        <p className="text-center mt-5">
+          Bạn có thể liên hệ admin để biết thêm chi tiết{" "}
+          <Link to="https://zalo.me/0394217863" target="_blank" rel="noopener noreferrer">
+            Qua zalo
+          </Link>
+        </p>
       </div>
     </>
   );
